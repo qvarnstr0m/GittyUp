20c301d3265035447867a2a7c54f40fb5ef121ae
qvarnstr0m <mqvarnstrom80@gmail.com>
2023-03-05 09:52:45 +0100

    Using arrow function

--- src/gittyup-get.js
+++ src/gittyup-get.js
@@ -1,5 +1,5 @@
 
-async function getLatestEvents(username, numEvents) {
+const getLatestEvents = async (username, numEvents) => {
     const response = await fetch(`https://api.github.com/users/${username}/events`);
     const events = await response.json();
     const latestEvents = events.slice(0, numEvents);
