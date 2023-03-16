7cfd1c615d793da65c69e25cc4e0a831ecc430c7
Abdul Alim <abdulalimrakib53@gmail.com>
2023-03-16 22:45:52 +0600

    loading and error handler added

--- gittyup-config.js
+++ gittyup-config.js
@@ -1,7 +1,7 @@
 // gittyup-config.js, set to your github username and preferences
 
 export const GittyupConfig = {
-    username: '<enter-username>',
-    numEvents: 5,
-    includeDateTime: false
+  username: 'no-alim',
+  numEvents: 5,
+  includeDateTime: false,
 };
--- src/gittyup-get.js
+++ src/gittyup-get.js
@@ -1,10 +1,35 @@
+const getLatestEvents = async (userName, numEvents) => {
+  let response;
+  let loading = true;
+  try {
+    response = await fetch(`https://api.github.com/users/${userName}/events`);
+    loading = false;
+  } catch (error) {
+    loading = false;
+  }
 
-const getLatestEvents = async (username, numEvents) => {
-    const response = await fetch(`https://api.github.com/users/${username}/events`);
+  //loading
+  if (loading) {
+    const eventsList = document.getElementById('loadingError');
+    const h5 = document.createElement('h5');
+    h5.innerHTML = `loading...`;
+    eventsList.appendChild(h5);
+    return [];
+  }
+
+  //check if there is an error
+  if (response?.ok) {
     const events = await response.json();
     const latestEvents = events.slice(0, numEvents);
     return latestEvents;
+  } else {
+    //here show the error
+    const eventsList = document.getElementById('loadingError');
+    const h5 = document.createElement('h5');
+    h5.innerHTML = `An Error Occurred. HTTP Response Code ${response?.status}`;
+    eventsList.appendChild(h5);
+    return [];
   }
-  
-  export { getLatestEvents };
-  
+};
+
+export { getLatestEvents };
